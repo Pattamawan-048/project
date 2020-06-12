59dacf145415dfcd0c27ad3131dbea5ca958b3ff
Pattamawan-048 <61343345+Pattamawan-048@users.noreply.github.com>
2020-06-12 23:59:32 +0700

    Update IR.ino

--- test/IR/IR.ino
+++ test/IR/IR.ino
@@ -1,3 +1,24 @@
-void setup();
+void setup(){
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(detect,INPUT);
+  pinMode(buzzer,OUTPUT);
+  pinMode(led,OUTPUT);
+}
+void loop() {
+  {
+  sensorValue = analogRead(detect);
+  Serial.println(sensorValue);
+  }
+  if (sensorValue<300)
+  digitalWrite (buzzer,HIGH);
+  else digitalWrite (buzzer,LOW);
+    if (sensorValue>300)
+  digitalWrite (led,LOW);
+  else digitalWrite (led,HIGH);
+  //Serial.println (mix);*/
+}
 
-void loop();
+
+
+
