3f8f6a747c495d575f573aa5297d3fbeaae8817c
Pattamawan-048 <61343345+Pattamawan-048@users.noreply.github.com>
2020-06-25 16:29:19 +0700

    Update LCD.ino

--- test/LCD/LCD.ino
+++ test/LCD/LCD.ino
@@ -8,7 +8,7 @@ int detect = 5;
 int buzzer = 8;
 int val = 0;
 int sensorValue; //ค่าที่จะนำไปแสดงผล
-
+int count=0;
 void setup() {
   lcd.init();
   Serial.begin(9600);
@@ -27,6 +27,7 @@ void loop() {
   Serial.println(distanceCm);
 
   IR();
+  Serial.println(count);
   lcd.setCursor(0, 0); // Sets the location at which subsequent text written to
                        // the LCD will be displayed
   lcd.print("Distance"); // Prints string "Distance" on the LCD
@@ -34,6 +35,7 @@ void loop() {
   lcd.print(distanceCm); // Prints the distance value from the sensor
   lcd.print(" cm");
   delay(10);
+  lcd.clear();
 }
 void IR() {
   if (distanceCm <= 5) {
@@ -41,85 +43,87 @@ void IR() {
     Serial.println(val);
     if (val > 1000) {
       //digitalWrite(buzzer, 0);
-      digitalWrite(2, 1);
-      
+      digitalWrite(2, 1); 
     } else {
       //digitalWrite(buzzer, 1);
-      digitalWrite(2, 0);
-       tone(buzzer, 300,10); 
-    delay(100); 
-     tone(buzzer,300,10); 
-    delay(100); 
+      //digitalWrite(2, 0);
+      /* tone(buzzer, 300,10); 
+      delay(100); 
+      tone(buzzer,300,10); 
+      delay(100); */
 
-pinMode(2, OUTPUT); // กำหนดขาทำหน้าที่ OUTPUT 
-pinMode(3, OUTPUT); 
-pinMode(4, OUTPUT); 
-pinMode(5, OUTPUT);
-pinMode(6, OUTPUT); 
-pinMode(7, OUTPUT); 
-pinMode(8, OUTPUT); 
-pinMode(9, OUTPUT); 
-pinMode(10, OUTPUT); 
-pinMode(11, OUTPUT); 
-pinMode(12, OUTPUT); 
-pinMode(13, OUTPUT); 
- {
-  if (digitalRead(2) == 1) {
-      digitalWrite(7,HIGH); // ไฟขา 7,8 ติด 100 ms 
-digitalWrite(8,HIGH);
-delay(30); 
-digitalWrite(7,LOW); // ไฟขา 7,8 ดับ 100 ms 
-digitalWrite(8,LOW);
-delay(30); 
-digitalWrite(6,HIGH);
-digitalWrite(9,HIGH); 
-delay(30); 
-digitalWrite(6,LOW); 
-digitalWrite(9,LOW); 
-delay(30); 
-digitalWrite(5,HIGH);
- digitalWrite(10,HIGH);
-delay(30); 
-digitalWrite(5,LOW); 
-digitalWrite(10,LOW); 
-delay(30); 
-digitalWrite(4,HIGH);
-digitalWrite(11,HIGH); 
-delay(30); 
-digitalWrite(4,LOW); 
-digitalWrite(11,LOW); 
-delay(30); 
-digitalWrite(3,HIGH); 
-digitalWrite(12,HIGH); 
-delay(30); 
-digitalWrite(3,LOW); 
-digitalWrite(12,LOW); 
-delay(30); 
-digitalWrite(13,HIGH); 
-delay(30); 
-digitalWrite(13,LOW); 
-delay(30); 
-  
-  } else {
-      digitalWrite(2, 0);
-      digitalWrite(3, 0);
-    digitalWrite(4, 0);
-    digitalWrite(5, 0);
-    digitalWrite(6, 0);
-    digitalWrite(7, 0);
-    digitalWrite(8, 0);
-    digitalWrite(9, 0);
-    digitalWrite(10, 0);
-    digitalWrite(11, 0);
-    digitalWrite(12, 0);
-    digitalWrite(13, 0);
-    
-    if (digitalRead(2) == 1) {
-       }
+      pinMode(2, INPUT_PULLUP); // กำหนดขาทำหน้าที่ OUTPUT 
+      pinMode(3, OUTPUT); 
+      pinMode(4, OUTPUT); 
+      pinMode(5, OUTPUT);
+      pinMode(6, OUTPUT); 
+      pinMode(7, OUTPUT); 
+      pinMode(8, OUTPUT); 
+      pinMode(9, OUTPUT); 
+      pinMode(10, OUTPUT); 
+      pinMode(11, OUTPUT); 
+      pinMode(12, OUTPUT); 
+      pinMode(13, OUTPUT); 
+     
+      if (digitalRead(2)==0) {
+      count ++;
       }
-     }
+      if(count >= 1){
+          digitalWrite(3, 0);
+          digitalWrite(4, 0);
+          digitalWrite(5, 0);
+          digitalWrite(6, 0);
+          digitalWrite(7, 0);
+          digitalWrite(8, 0);
+          digitalWrite(9, 0);
+          digitalWrite(10, 0);
+          digitalWrite(11, 0);
+          digitalWrite(12, 0);
+          digitalWrite(13, 0);
+      }
+      else if (count == 0) {
+          tone(buzzer, 300,10); 
+          delay(100); 
+          tone(buzzer,300,10); 
+          delay(100); 
+          digitalWrite(7,HIGH); // ไฟขา 7,8 ติด 100 ms 
+          digitalWrite(8,HIGH);
+          delay(30); 
+          digitalWrite(7,LOW); // ไฟขา 7,8 ดับ 100 ms 
+          digitalWrite(8,LOW);
+          delay(30); 
+          digitalWrite(6,HIGH);
+          digitalWrite(9,HIGH); 
+          delay(30); 
+          digitalWrite(6,LOW); 
+          digitalWrite(9,LOW); 
+          delay(30); 
+          digitalWrite(5,HIGH);
+          digitalWrite(10,HIGH);
+          delay(30); 
+          digitalWrite(5,LOW); 
+          digitalWrite(10,LOW); 
+          delay(30); 
+          digitalWrite(4,HIGH);
+          digitalWrite(11,HIGH); 
+          delay(30); 
+          digitalWrite(4,LOW); 
+          digitalWrite(11,LOW); 
+          delay(30); 
+          digitalWrite(3,HIGH); 
+          digitalWrite(12,HIGH); 
+          delay(30); 
+          digitalWrite(3,LOW); 
+          digitalWrite(12,LOW); 
+          delay(30); 
+          digitalWrite(13,HIGH); 
+          delay(30); 
+          digitalWrite(13,LOW); 
+          delay(30);
+      }
     }
   }
+  
 }
 
   
